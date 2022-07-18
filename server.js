2ac744772ee702102f34765c0a556a6ebb3c0110
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-07-18 17:31:08 -0400

    added function to create new notes

--- server.js
+++ server.js
@@ -11,6 +11,29 @@ const PORT = process.env.PORT || 3001;
 //instantiate the server
 const app = express();
 
+//Middleware Functions
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+// End of Middleware
+
+function createNewNote(body, notesArray) {
+  console.log(body);
+  const note = body;
+  notesArray.push(notes);
+  
+  //path to write file
+  fs.writeFileSync(
+    path.join(__dirname, './data/db.json'),
+    JSON.stringify({ notes : notesArray }, null, 2)
+  );
+
+  //return finished code to post route for response
+  return note;
+};
+
+
 //route GET
 app.get('/api/notes', (req, res) => {
   res.json(notes);
@@ -26,6 +49,13 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname,'./public/notes.html'));
 }); 
+
+//POST Routes
+app.post('/api/notes', (req, res) => {
+  //req.body is where incoming content will be
+  console.log(req.body);
+  res.json(req.body)
+});
 
 
 //make server listen
